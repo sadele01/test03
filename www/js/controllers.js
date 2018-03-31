b34844fcd8d15ba3fe1fa43480b4fdb599584f5c
sadele01 <34429945+sadele01@users.noreply.github.com>
2018-03-31 09:15:38 -0600

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -367,9 +367,21 @@ angular.module('starter.controllers', ['ionic'])
 
         })
 
-        .controller('MyPointsCtrl', function ($scope, SalBr) {
+        .controller('MyPointsCtrl', function ($scope, SalBr, $http) {
+                myServiceId = {
+                        page: 1,
+                        data: [],
+                        total_pages: 0,
+                        total_lines:0
+                };
+                
+                mygcm = {
+                         user_key: null
+                };
+
                 SalBr.fetchMyServiceId();
                 $scope.SalBr = SalBr;
+                
             console.log('my points');
         })
 
