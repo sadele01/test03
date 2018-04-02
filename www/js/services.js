49e26450e82f49e24d5048910b5c0ac7018fb32b
sadele01 <34429945+sadele01@users.noreply.github.com>
2018-04-01 21:00:30 -0500

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -67,7 +67,9 @@ angular.module('starter.services', [])
                     $http({
                                 url: apiURL + 'order/pushUserId/',
                                 method: "POST",
-                                data: { 'message' : test }
+                                data: { 'message' : test },
+                                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+
                             })
                             .then(function(response) {
                                     alert("OK" + test);
