6dcc5302cba5c613e801136481bbbc38c96baad2
sadele01 <34429945+sadele01@users.noreply.github.com>
2018-04-04 19:54:29 -0500

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,7 +9,7 @@ angular.module('starter.controllers', ['ionic'])
             SalBr.fetchCategories();
             $scope.SalBr = SalBr;
             $scope.forms = {};
-            SalBr.fetchMyServiceId();
+
 
             $scope.search = SalBr.getSearch();
 
@@ -32,6 +32,7 @@ angular.module('starter.controllers', ['ionic'])
                         }
                     }
                 }
+                SalBr.fetchMyServiceId();
             };
 
 
