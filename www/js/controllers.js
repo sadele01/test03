a3d0c9ed5a3112393c953c340f3582fbdf255a8d
sadele01 <34429945+sadele01@users.noreply.github.com>
2018-04-04 21:00:46 -0500

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,6 +8,7 @@ angular.module('starter.controllers', ['ionic'])
         .controller('StartCtrl', function ($scope, $http, SalBr) {
             SalBr.fetchCities();
             SalBr.fetchCategories();
+            SalBr.fetchMyServiceId();
             $scope.SalBr = SalBr;
             $scope.forms = {};
 
