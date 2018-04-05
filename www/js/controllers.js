45c7930ec2fea1da7228efc49307abc75a738ed4
sadele01 <34429945+sadele01@users.noreply.github.com>
2018-04-05 04:27:17 -0500

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,7 +14,7 @@ angular.module('starter.controllers', ['ionic'])
             $scope.SalBr = SalBr;
             $scope.forms = {};
         
-            SalBr.getMyGcm();
+            //SalBr.getMyGcm();
             //SalBr.sendMyPushId(test);
 
 
