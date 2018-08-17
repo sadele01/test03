68f5a861227d5c1fc1975b671541c6ae194a4841
sadele01 <34429945+sadele01@users.noreply.github.com>
2018-08-16 23:31:49 -0500

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', ['ionic','ionic-modal-select'])
+angular.module('starter.controllers', ['ionic'])
 
         .controller('mainController', function ($scope, SalBr) {
             SalBr.recoverSession();
